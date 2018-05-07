5ca2709922dc20cb6a4c09d62d84b7b07c13397b
Michael Schinagl <michael.schinagl@fh-hagenberg.at>
2018-05-07 11:38:13 +0200

    added some LoC with result 42

--- kast_schinagl_android/src/dummy_project/DummyClass.java
+++ kast_schinagl_android/src/dummy_project/DummyClass.java
@@ -7,5 +7,13 @@ public class DummyClass {
 		// This Method is empty!!
 		int i = 0;
 		i=1;
+		
+		//hello this is comment
+		
+		int m=11;
+		int n=31;
+		
+		System.out.println("result is: " + (n+m)); // -->42
+		
 	}
 }
